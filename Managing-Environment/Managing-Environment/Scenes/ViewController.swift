37ea61ced3126489c96a2b04f0d8087bd962c323
Bertuğ YILMAZ <ylmazbertug@gmail.com>
2020-02-03 15:53:21 +0300

    Initialize project

---
+++ Managing-Environment/Managing-Environment/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Managing-Environment
+//
+//  Created by Bertug YILMAZ on 3.02.2020.
+//  Copyright © 2020 bertugyilmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
