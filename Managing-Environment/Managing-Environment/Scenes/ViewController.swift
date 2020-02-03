c05ee0299b4a97b874745506f8281813bf0cd1a9
Bertuğ YILMAZ <ylmazbertug@gmail.com>
2020-02-03 18:44:14 +0300

    Added some sample for the usage

--- Managing-Environment/Managing-Environment/Scenes/ViewController.swift
+++ Managing-Environment/Managing-Environment/Scenes/ViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private let environment = Environment.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let environmentName: String = try! environment.configuration(.environmentName)
+        let baseUrl: String = try! environment.configuration(.baseUrl)
+        
+        print(environmentName)
+        print(baseUrl.replacingOccurrences(of: "\\", with: ""))
+        
+//        let missingValue: String = try! environment.configuration(.appIconName)
+//        print(missingValue)
     }
 }
 
